383f38b3e087fcf73c4dac57cfb50aa7d24a7fba
Muhammad Sameem <sameemgujjar123@yahoo.com>
2024-02-19 13:56:27 +0400

    :heavy_check_mark: Variables and Operators done

---
+++ JavaScript/variables-and-operators/script.js
@@ -0,0 +1,61 @@
+"use strict";
+// // Working with variables
+// let admin;
+// let name;
+// name = "John";
+// admin = name;
+// alert(admin);
+
+// // Giving the right name
+// let ourPlanetName = "Earth";
+// let currentUserName = "John";
+
+// // Uppercase const
+
+// const BIRTHDAY = "18.04.1982";
+
+// // const age = someCode(BIRTHDAY);
+
+// // The postfix and prefix forms
+
+// let a = 1,
+//   b = 1;
+
+// let c = ++a; // 2
+// let d = b++; // 1
+
+// alert(c, d);
+
+// //
+// let a1 = 2;
+
+// let x = 1 + (a1 *= 2);
+
+// console.log(x);
+
+// let a2 = +prompt("First number?", 1);
+// let b2 = +prompt("Second number?", 2);
+
+// alert(a2 + b2); // 12
+
+// The Odin Project - exercises
+// 1-
+console.log(15 + 22);
+
+// 2-
+console.log(28 + 63 + 76 + 168 + 65 + 42);
+
+// 3-
+console.log((4 + 6 + 9) / 77);
+
+// 4-
+let a = 10;
+console.log(a);
+console.log(9 * a);
+let b = 7 * a;
+console.log(b);
+
+// 5-
+const MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;
