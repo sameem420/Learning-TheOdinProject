ab53af4e19a6d3499416faa19ef5c54cfa7649c8
Muhammad Sameem <sameemgujjar123@yahoo.com>
2024-03-11 15:45:55 +0400

    :heavy_check_mark: exercise remove from array

--- JavaScript/javascript-exercises/04_removeFromArray/removeFromArray.js
+++ JavaScript/javascript-exercises/04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (nums, ...args) {
+  let numsArr = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (!args.includes(nums[i])) {
+      numsArr.push(nums[i]);
+    }
+  }
+  return numsArr;
 };
+
+// var removeFromArray = function(array, ...args) {
+//   return array.filter(val => !args.includes(val))
+// }
+//
 
 // Do not edit below this line
 module.exports = removeFromArray;
