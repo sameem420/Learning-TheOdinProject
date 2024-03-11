bf725c2205eb66f777554b0627c9c88b5734fae8
Muhammad Sameem <sameemgujjar123@yahoo.com>
2024-03-11 15:27:25 +0400

    :heavy_check_mark: exercise reverse-string

--- JavaScript/javascript-exercises/02_repeatString/repeatString.js
+++ JavaScript/javascript-exercises/02_repeatString/repeatString.js
@@ -1,9 +1,13 @@
 const repeatString = function (word, times) {
   if (times < 0) return "ERROR";
-
-  return word.repeat(times);
+  let str = "";
+  for (let i = 0; i < times; i++) {
+    str += word;
+  }
+  return str;
+  // possible solution also without loops
+  // return word.repeat(times);
 };
 
-console.log(repeatString("", 10));
 // Do not edit below this line
 module.exports = repeatString;
--- JavaScript/javascript-exercises/03_reverseString/reverseString.js
+++ JavaScript/javascript-exercises/03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
 const reverseString = function (str) {
-  return str.reverse();
+  let reversed = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    reversed += str[i];
+  }
+  return reversed;
+  // possible solution without loops
+  // return str.split("").reverse().join("");
 };
 
+console.log(reverseString("!cba !321"));
 // Do not edit below this line
 module.exports = reverseString;
