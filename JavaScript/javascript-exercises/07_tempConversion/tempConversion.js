8aee545d48fffefecc8f00dc60a4440641962e2a
Muhammad Sameem <sameemgujjar123@yahoo.com>
2024-03-12 10:01:32 +0400

    :heavy_check_mark: exercise temperature conversion

--- JavaScript/javascript-exercises/07_tempConversion/tempConversion.js
+++ JavaScript/javascript-exercises/07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (temperatueInFahren) {
+  return +((temperatueInFahren - 32) * (5 / 9)).toFixed(1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (temperatueInCelsius) {
+  return +((temperatueInCelsius * 9) / 5 + 32).toFixed(1);
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
