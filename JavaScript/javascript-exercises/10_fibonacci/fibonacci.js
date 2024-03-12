59a8f303b18be2daabdb49266c15b31dc54aace8
Muhammad Sameem <sameemgujjar123@yahoo.com>
2024-03-12 11:33:25 +0400

    :heavy_check_mark: exercise fibonacci series

--- JavaScript/javascript-exercises/10_fibonacci/fibonacci.js
+++ JavaScript/javascript-exercises/10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
-
+const fibonacci = function (count) {
+  if (count < 0) return "OOPS";
+  if (count == 0) return 0;
+  count = parseInt(count);
+  let n1 = 1,
+    n2 = 0,
+    nextTerm;
+  for (let i = 2; i <= count; i++) {
+    nextTerm = n1 + n2;
+    n2 = n1;
+    n1 = nextTerm;
+  }
+  return n1;
 };
 
 // Do not edit below this line
